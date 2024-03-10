3405696a7b71f993112904a019837c216d2e8a9f
songjaeheon <spes2you@gmail.com>
2024-03-11 00:27:54 +0900

    feat: Spring Initializr

---
+++ src/main/java/com/footballmania/wascommunity/WasCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.footballmania.wascommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/footballmania/wascommunity/WasCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballmania.wascommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
